76505f8e50f9e704ea3c7149fc0dbcf9cfe13039
Riaz <muhammedriaz95@gmail.com>
2024-03-31 13:55:21 +0400

    fix movie tile

--- src/components/MovieTile.tsx
+++ src/components/MovieTile.tsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom";
+import moment from "moment";
 
 export interface movieDb {
     id: number;
@@ -9,11 +10,14 @@ export interface movieDb {
 
 export default function MovieTile({data}: {data: movieDb}) {
     return (
-        <Link to={`movie/${data.id}`} className="flex flex-col w-full" key={data.id}>
-            <img className="h-48" src={`https://media.themoviedb.org/t/p/w440_and_h660_face/${data.poster_path}`}
-                 alt={data.title}/>
-            <h3 className="text-sm">{data.title}</h3>
-            <h4 className="text-xs text-gray-500">{data.release_date}</h4>
+        <Link to={`movie/${data.id}`} className="flex flex-col" key={data.id}>
+            <div className="h-56 w-36">
+                <img className="h-full w-full"
+                     src={`https://media.themoviedb.org/t/p/w440_and_h660_face/${data.poster_path}`}
+                     alt={data.title}/>
+            </div>
+            <h3 className="text-sm text-white">{data.title}</h3>
+            <h4 className="text-xs text-gray-300">{moment(data.release_date).format('LL').toString()}</h4>
         </Link>
     )
 }
