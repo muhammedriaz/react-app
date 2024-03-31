1dc936a9beb8632d505b3c2543049c7862b295a5
Riaz <muhammedriaz95@gmail.com>
2024-03-31 13:55:57 +0400

    add movie details page

---
+++ src/utils/helpers.ts
@@ -0,0 +1,5 @@
+export const getMovieTime = (minutes: number) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours}h ${remainingMinutes}min`;
+}
